5adaf2241968396ab97810726517cb626533e5a5
Erwin Flores <ramiromarca6@gmail.com>
2020-12-22 16:18:31 -0400

    ultimos cambios

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder:{
+            builderOptions: {
+                productName: 'Planillas Cooperativa 18 de Julio R.L.',
+                "nsis": {
+                    "allowElevation": false,
+                    "allowToChangeInstallationDirectory": true,
+                    "oneClick": false,
+                    "perMachine": true
+                }
+            }
+        }
+    }
+}
